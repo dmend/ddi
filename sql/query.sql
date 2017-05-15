a64b957aaf20acc8126194fdbd97ee381824ab77
Douglas Mendizábal <douglas@redrobot.io>
2017-05-15 00:03:25 -0500

    Don't use subqueries.

--- sql/query.sql
+++ sql/query.sql
@@ -1,7 +1,3 @@
-SELECT v.VULN_ID, p.PAGE_ID, v.data FROM (
-    SELECT * FROM websites WHERE url LIKE '%foo.com'
-) AS w
-    JOIN (
-        SELECT * FROM pages WHERE path LIKE '%login.html'
-) AS p ON w.WEBSITE_ID=p.WEBSITE_ID
-    JOIN vulnerabilities v ON p.PAGE_ID=v.PAGE_ID;
+SELECT v.VULN_ID, p.PAGE_ID, v.data FROM websites w
+    JOIN pages p on w.WEBSITE_ID=p.WEBSITE_ID AND w.url LIKE '%foo.com'
+    JOIN vulnerabilities v ON p.PAGE_ID=v.PAGE_ID AND p.path LIKE '%login.html';
